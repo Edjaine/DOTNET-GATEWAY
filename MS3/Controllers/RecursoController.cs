4815657bca49c410102bd934f7e2c87b37769e84
Edjaine <c83edsoliveira@hotmail.com>
2020-08-02 17:31:40 -0300

    Inicial

---
+++ MS3/Controllers/RecursoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MS3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RecursoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Microservice 0003") ;
+        }
+    }
+}
